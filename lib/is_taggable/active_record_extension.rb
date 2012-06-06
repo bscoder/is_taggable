e3abc879ad331e730f667879c7122d4e74b1db14
Alexey Smolianinov <alexisowl@gmail.com>
2012-06-07 03:36:04 +0400

    cosmetic internal changes

--- lib/is_taggable/active_record_extension.rb
+++ lib/is_taggable/active_record_extension.rb
@@ -4,8 +4,8 @@ module IsTaggable
       class_attribute :tag_kinds
 
       kinds = [:tags]  if kinds.empty?
-      tag_kinds_old = tag_kinds || []
-      self.tag_kinds = tag_kinds_old | kinds.map(&:to_s).map(&:singularize)
+      inherited_kinds = tag_kinds || []
+      self.tag_kinds = inherited_kinds | kinds.map(&:to_s).map(&:singularize)
 
       include IsTaggable::Mixin
     end
