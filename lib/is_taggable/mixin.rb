a037a6f826361e9429db7c92c4e2572b43428793
Alexey Smolianinov <alexisowl@gmail.com>
2012-06-08 02:48:59 +0400

    removed redundant pieces

--- lib/is_taggable/mixin.rb
+++ lib/is_taggable/mixin.rb
@@ -55,12 +55,9 @@ module IsTaggable
 
         def add_new_tags(tag_kind)
           get_tag_list(tag_kind).each do |tag_name| 
-            # Let Tag normalize the tag name.
             tag = Tag.find_or_initialize_with_name_like_and_kind(tag_name, tag_kind)
             tags << tag unless tags.include?(tag)
           end
-          # BUGGY Remember the normalized tag names.
-          # set_tag_list(tag_kind, tags.of_kind(tag_kind).map(&:name))
         end
     end
   end
--- lib/tag.rb
+++ lib/tag.rb
@@ -1,10 +1,4 @@
 class Tag < ActiveRecord::Base
-  class << self
-    def find_or_initialize_with_name_like_and_kind(name, kind)
-      name = collapse_whitespace(name)
-      with_name_like_and_kind(name, kind).first || new(:name => name, :kind => kind)
-    end
-  end
 
   has_many :taggings, :dependent => :destroy
 
@@ -13,14 +7,9 @@ class Tag < ActiveRecord::Base
 
   scope :with_name_like_and_kind, lambda { |name, kind| { :conditions => ["name like ? AND kind = ?", name, kind] } }
   scope :of_kind,                 lambda { |kind| { :conditions => {:kind => kind} } }
-  
-  def name=(name)
-    super self.class.collapse_whitespace(name)
+
+  def self.find_or_initialize_with_name_like_and_kind(name, kind)
+    with_name_like_and_kind(name, kind).first || new(:name => name, :kind => kind)
   end
 
-protected
-  
-  def self.collapse_whitespace(name)
-    name.gsub(/\s+/, ' ').strip
-  end
 end
