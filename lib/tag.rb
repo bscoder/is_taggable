9cd9d7e5a963f9c49dc2d19dae5d2176f3e85fb4
Alexey Smolianinov <alexisowl@gmail.com>
2012-06-11 17:59:37 +0400

    switched to Arel syntax

--- lib/tag.rb
+++ lib/tag.rb
@@ -5,8 +5,8 @@ class Tag < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name, :scope => :kind
 
-  scope :with_name_like_and_kind, lambda { |name, kind| { :conditions => ["name like ? AND kind = ?", name, kind] } }
-  scope :of_kind,                 lambda { |kind| { :conditions => {:kind => kind} } }
+  scope :with_name_like_and_kind, lambda { |name, kind| where("name like ? AND kind = ?", name, kind) }
+  scope :of_kind,                 lambda { |kind| where(:kind => kind) }
 
   def self.find_or_initialize_with_name_like_and_kind(name, kind)
     with_name_like_and_kind(name, kind).first || new(:name => name, :kind => kind)
